c84b88dcd835451c5807768af7bef4323bd21e23
Festus Agboma <festusyuma@gmail.com>
2020-04-28 18:57:21 +0100

    Initialize glaid backend

---
+++ src/main/kotlin/festusyuma/com/glaid/GlaidApplication.kt
@@ -0,0 +1,11 @@
+package festusyuma.com.glaid
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GlaidApplication
+
+fun main(args: Array<String>) {
+	runApplication<GlaidApplication>(*args)
+}
---
+++ src/test/kotlin/festusyuma/com/glaid/GlaidApplicationTests.kt
@@ -0,0 +1,13 @@
+package festusyuma.com.glaid
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GlaidApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
