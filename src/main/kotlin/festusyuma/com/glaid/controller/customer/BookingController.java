bcf708087dcb5713a325488124b326de831af19c
Festus Agboma <festusyuma@gmail.com>
2020-04-28 19:35:58 +0100

    Created Customer controller files

--- src/main/kotlin/festusyuma/com/glaid/controller/customer/BookingController.java
+++ src/main/kotlin/festusyuma/com/glaid/controller/customer/BookingController.java
@@ -1,6 +1,5 @@
 package festusyuma.com.glaid.controller.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("customer/booking")
