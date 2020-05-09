22340233df5cff67ac369fad601a3a5557aac2f1
Festus Agboma <festusyuma@gmail.com>
2020-05-09 15:51:33 +0100

    created custom security user details service

--- src/main/kotlin/festusyuma/com/glaid/security/UserDetailsService.kt
+++ src/main/kotlin/festusyuma/com/glaid/security/UserDetailsService.kt
@@ -1,7 +1,18 @@
 package festusyuma.com.glaid.security
 
+import festusyuma.com.glaid.model.User
+import festusyuma.com.glaid.repository.UserRepo
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class UserDetailsService {
+class UserDetailsService(private val userRepo: UserRepo): UserDetailsService {
+
+    override fun loadUserByUsername(email: String): UserDetails? {
+        val user: User? = userRepo.findByEmail(email)
+
+        return if (user != null) {
+            UserDetails(user)
+        }else user
+    }
 }
