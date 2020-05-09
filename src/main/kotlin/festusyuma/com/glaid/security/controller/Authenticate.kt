9d6abd97a7ca25fcf9a91e4237fe5dc35de73103
Festus Agboma <festusyuma@gmail.com>
2020-05-09 18:16:30 +0100

    updated authenticate controller

--- src/main/kotlin/festusyuma/com/glaid/security/controller/Authenticate.kt
+++ src/main/kotlin/festusyuma/com/glaid/security/controller/Authenticate.kt
@@ -3,6 +3,10 @@ package festusyuma.com.glaid.security.controller
 import festusyuma.com.glaid.security.UserDetailsService
 import festusyuma.com.glaid.security.model.AuthenticateRequest
 import festusyuma.com.glaid.security.utl.JWTUtil
+import festusyuma.com.glaid.util.Response
+import festusyuma.com.glaid.util.response
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,13 +22,17 @@ class Authenticate (
 ) {
 
     @PostMapping("/login")
-    fun login(@RequestBody req: AuthenticateRequest): String {
+    fun login(@RequestBody req: AuthenticateRequest): ResponseEntity<Response> {
         try {
             authManager.authenticate(UsernamePasswordAuthenticationToken(req.email, req.password))
         }catch (e: Exception) {
-            return ""
+            return response(HttpStatus.UNAUTHORIZED, "Incorrect email or password")
         }
 
-        return ""
+        val userDetails = userDetailsService.loadUserByUsername(req.email)
+
+        return if (userDetails != null) {
+            response(message = "Login successful", data = mapOf("token" to jwtUtil.generateToken(userDetails)))
+        }else response(HttpStatus.UNAUTHORIZED, "Incorrect email or password")
     }
 }
