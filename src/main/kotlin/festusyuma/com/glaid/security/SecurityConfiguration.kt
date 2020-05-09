84a0e6bfadcb1f37f50a157758d7e4148a3e5fc1
Festus Agboma <festusyuma@gmail.com>
2020-05-09 17:38:10 +0100

    created authenticate response util

--- src/main/kotlin/festusyuma/com/glaid/security/SecurityConfiguration.kt
+++ src/main/kotlin/festusyuma/com/glaid/security/SecurityConfiguration.kt
@@ -1,7 +1,15 @@
 package festusyuma.com.glaid.security
 
+import org.springframework.context.annotation.Bean
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration: WebSecurityConfigurerAdapter() {
+
+    @Bean
+    override fun authenticationManager(): AuthenticationManager {
+        return super.authenticationManager()
+    }
 }
--- src/main/kotlin/festusyuma/com/glaid/security/controller/Authenticate.kt
+++ src/main/kotlin/festusyuma/com/glaid/security/controller/Authenticate.kt
@@ -1,9 +1,30 @@
 package festusyuma.com.glaid.security.controller
 
+import festusyuma.com.glaid.security.UserDetailsService
+import festusyuma.com.glaid.security.model.AuthenticateRequest
+import festusyuma.com.glaid.security.utl.JWTUtil
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.lang.Exception
 
 @RestController
 class Authenticate (
+        private val authManager: AuthenticationManager,
+        private val userDetailsService: UserDetailsService,
+        private val jwtUtil: JWTUtil
 ) {
+
+    @PostMapping("/login")
+    fun login(@RequestBody req: AuthenticateRequest): String {
+        try {
+            authManager.authenticate(UsernamePasswordAuthenticationToken(req.email, req.password))
+        }catch (e: Exception) {
+            return ""
+        }
+
+        return ""
+    }
 }
