7ed25dbed75c8280c862b94d1d6f1ad30dd896b5
Festus Agboma <festusyuma@gmail.com>
2020-07-10 19:55:23 +0100

    driver dashboard

--- src/main/kotlin/festusyuma/com/glaid/controller/driver/DashboardController.kt
+++ src/main/kotlin/festusyuma/com/glaid/controller/driver/DashboardController.kt
@@ -1,18 +1,32 @@
 package festusyuma.com.glaid.controller.driver
 
+import festusyuma.com.glaid.service.DriverService
 import festusyuma.com.glaid.util.Response
 import festusyuma.com.glaid.util.response
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("driver")
-@RequestMapping("driver")
-class DashboardController {
+@RestController("driver/dashboard")
+@RequestMapping("driver/dashboard")
+class DashboardController(
+        private val driverService: DriverService
+) {
 
     @GetMapping
     fun home(): ResponseEntity<Response> {
-        return response(message = "Driver Welcome")
+        val customer = driverService.getLoggedInDriver()?:
+        return response(HttpStatus.BAD_REQUEST, "an error occurred")
+
+        return response(message = "Customer Welcome", data = customer)
+    }
+
+    @GetMapping("validate_token")
+    fun validateToken(): ResponseEntity<Response> {
+        return if (driverService.getLoggedInDriver() == null) {
+            response(HttpStatus.BAD_REQUEST, "an error occurred")
+        }else response()
     }
 }
