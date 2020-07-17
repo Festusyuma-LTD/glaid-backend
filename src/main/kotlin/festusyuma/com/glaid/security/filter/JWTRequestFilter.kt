dcdb0de0f7310e102680a46cd2e7a785a57b07b4
Festus Agboma <festusyuma@gmail.com>
2020-07-17 19:16:27 +0100

    migrate to google custom authentication

--- src/main/kotlin/festusyuma/com/glaid/security/filter/JWTRequestFilter.kt
+++ src/main/kotlin/festusyuma/com/glaid/security/filter/JWTRequestFilter.kt
@@ -1,5 +1,8 @@
 package festusyuma.com.glaid.security.filter
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import festusyuma.com.glaid.security.UserDetails
 import festusyuma.com.glaid.security.model.JWTTokenRepo
 import festusyuma.com.glaid.security.utl.JWTUtil
@@ -8,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
+import java.io.FileInputStream
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -18,17 +22,29 @@ class JWTRequestFilter(
         private val jwtTokenRepo: JWTTokenRepo
 ): OncePerRequestFilter() {
 
+    private lateinit var fireBaseApp: FirebaseApp
+
     override fun doFilterInternal(req: HttpServletRequest, res: HttpServletResponse, chain: FilterChain) {
+
+        if (FirebaseApp.getApps().size == 0) {
+            val serviceAccount = FileInputStream("C:/Users/festu/Documents/Work/glaid project/backend/glaid/src/main/resources/glaid-tracking-firebase-adminsdk.json")
+            val options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://glaid-tracking.firebaseio.com/")
+                    .build()
+
+            fireBaseApp = FirebaseApp.initializeApp(options)
+        }
+
 
         val authorization: String? = req.getHeader("Authorization")
         if (authorization?.startsWith("Bearer ") == true) {
-            val token = jwtTokenRepo.findByToken(authorization.substring(7))
+            val token = jwtTokenRepo.findByTokenAndExpired(authorization.substring(7))
 
             if (token != null) {
                 val user = token.user
-                val tokenActive = !token.expired
 
-                if (tokenActive && user != null) {
+                if (user != null) {
                     if (jwtUtil.validateToken(token, user.email)) {
                         val userDetails = UserDetails(user)
                         val userAuthentication = UsernamePasswordAuthenticationToken(
