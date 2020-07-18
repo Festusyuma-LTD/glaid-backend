bc7c5e51aba98293b3c719f7b04a788ca844540b
Festus Agboma <festusyuma@gmail.com>
2020-07-19 00:35:07 +0100

    add pending order to firestore

--- src/main/kotlin/festusyuma/com/glaid/util/Constants.kt
+++ src/main/kotlin/festusyuma/com/glaid/util/Constants.kt
@@ -1,5 +1,9 @@
 package festusyuma.com.glaid.util
 
+import com.google.firebase.cloud.FirestoreClient
+
+val db = FirestoreClient.getFirestore()
+
 const val USERS = "Users"
 const val USER_LOCATIONS = "User Locations"
 const val PENDING_ORDERS = "Pending Orders"
