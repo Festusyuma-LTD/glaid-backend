62267e97c6ef961cb60b7189c288fb2018d38054
Festus Agboma <festusyuma@gmail.com>
2020-07-21 00:34:53 +0100

    same name controller erro fix

--- src/main/kotlin/festusyuma/com/glaid/controller/driver/BookingController.kt
+++ src/main/kotlin/festusyuma/com/glaid/controller/driver/BookingController.kt
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("driver/dashboard")
-@RequestMapping("driver/dashboard")
+@RestController("driver/booking")
+@RequestMapping("driver/booking")
 class BookingController(
         private val orderService: OrderService
 ) {
