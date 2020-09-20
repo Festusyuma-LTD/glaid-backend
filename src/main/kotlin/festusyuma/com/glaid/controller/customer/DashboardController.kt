fed9e0941ba5c1ecc029f18d12ff1249ee482e97
Festus agboma <festusyuma@gmail.com>
2020-09-20 23:57:56 +0100

    send gastpye with dashboard request

--- src/main/kotlin/festusyuma/com/glaid/controller/customer/DashboardController.kt
+++ src/main/kotlin/festusyuma/com/glaid/controller/customer/DashboardController.kt
@@ -1,5 +1,6 @@
 package festusyuma.com.glaid.controller.customer
 
+import festusyuma.com.glaid.repository.GasRepo
 import festusyuma.com.glaid.service.CustomerService
 import festusyuma.com.glaid.util.Response
 import festusyuma.com.glaid.util.response
@@ -12,15 +13,21 @@ import org.springframework.web.bind.annotation.RestController
 @RestController("customer/dashboard")
 @RequestMapping("customer/dashboard")
 class DashboardController(
-        private val customerService: CustomerService
+        private val customerService: CustomerService,
+        private val gasRepo: GasRepo
 ) {
 
     @GetMapping
     fun home(): ResponseEntity<Response> {
         val customer = customerService.getLoggedInCustomer()?:
                 return response(HttpStatus.BAD_REQUEST, "an error occurred")
+        val gasType = gasRepo.findByTypeIn(listOf("diesel", "gas"));
+        val data = mapOf<String, Any>(
+                "customer" to customer,
+                "gasType" to gasType
+        )
 
-        return response(message = "Customer Welcome", data = customer)
+        return response(message = "Customer Welcome", data = data)
     }
 
     @GetMapping("validate_token")
