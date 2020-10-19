83afe7e4a5fadd9078472663feb15da9a1ea7201
Festus agboma <festusyuma@gmail.com>
2020-10-19 09:38:43 +0100

    verifiy id token

--- src/main/kotlin/festusyuma/com/glaid/security/controller/Authenticate.kt
+++ src/main/kotlin/festusyuma/com/glaid/security/controller/Authenticate.kt
@@ -1,22 +1,26 @@
 package festusyuma.com.glaid.security.controller
 
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
+import com.google.api.client.json.jackson2.JacksonFactory
 import festusyuma.com.glaid.dto.UserOTPRequest
 import festusyuma.com.glaid.repository.CustomerRepo
 import festusyuma.com.glaid.repository.DriverRepo
 import festusyuma.com.glaid.repository.RoleRepo
 import festusyuma.com.glaid.repository.UserRepo
+import festusyuma.com.glaid.security.UserDetails
 import festusyuma.com.glaid.security.UserDetailsService
 import festusyuma.com.glaid.security.UserPasswordService
 import festusyuma.com.glaid.security.model.AuthenticateRequest
 import festusyuma.com.glaid.security.model.PasswordUpdateRequest
 import festusyuma.com.glaid.security.utl.JWTUtil
 import festusyuma.com.glaid.security.utl.PasswordResetRequest
-import festusyuma.com.glaid.service.CustomerService
-import festusyuma.com.glaid.service.DriverService
 import festusyuma.com.glaid.service.UserOTPService
 import festusyuma.com.glaid.util.ERROR_OCCURRED_MSG
 import festusyuma.com.glaid.util.Response
 import festusyuma.com.glaid.util.response
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -27,10 +31,11 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import java.lang.Exception
+import java.util.*
+
 
 @RestController
-class Authenticate (
+class Authenticate(
         private val authManager: AuthenticationManager,
         private val userDetailsService: UserDetailsService,
         private val userPasswordService: UserPasswordService,
@@ -43,15 +48,43 @@ class Authenticate (
         private val roleRepo: RoleRepo
 ) {
 
+    @Value("\${CLIENT_ID}")
+    lateinit var clientId: String
+
+    @Value("\${CLIENT_SECRET}")
+    lateinit var clientSecret: String
+
     @PostMapping("/login")
     fun login(@RequestBody req: AuthenticateRequest): ResponseEntity<Response> {
-        try {
-            authManager.authenticate(UsernamePasswordAuthenticationToken(req.email, req.password))
-        }catch (e: Exception) {
-            return response(HttpStatus.UNAUTHORIZED, "Incorrect email or password")
+
+        var userDetails: UserDetails? = null
+
+        //todo handle google and facebook login
+        when(req.loginType) {
+            "email" -> {
+                try {
+                    authManager.authenticate(UsernamePasswordAuthenticationToken(req.email, req.password))
+                    userDetails = userDetailsService.loadUserByUsername(req.email)
+                } catch (e: Exception) {
+                    return response(HttpStatus.UNAUTHORIZED, "Incorrect email or password")
+                }
+            }
+            "google" -> {
+                val token = req.token
+                if (token != null) {
+                    val payload = googleSignIn(token)
+                            ?: return response(HttpStatus.UNAUTHORIZED, "Incorrect email or password")
+
+                    userDetails = userDetailsService.loadUserByUsername(payload.email)
+                    if (userDetails == null) {
+                        createAccountFromGooglePayload(payload, req.role)
+                    }
+                } else return response(HttpStatus.UNAUTHORIZED, "Incorrect email or password")
+            }
+            "facebook" -> {
+            }
         }
 
-        val userDetails = userDetailsService.loadUserByUsername(req.email)
 
         return if (userDetails != null) {
             val driverRole = roleRepo.findByIdOrNull(2)?: response(HttpStatus.BAD_REQUEST, ERROR_OCCURRED_MSG)
@@ -71,6 +104,42 @@ class Authenticate (
                     )
             )
         }else response(HttpStatus.UNAUTHORIZED, "Incorrect email or password")
+    }
+
+    private fun googleSignIn(token: String): GoogleIdToken.Payload? {
+        val jsonFactory = JacksonFactory()
+        val verifier = GoogleIdTokenVerifier.Builder(UrlFetchTransport.getDefaultInstance(), jsonFactory)
+                .setAudience(Collections.singletonList(clientId))
+                .build()
+
+        val idToken = verifier.verify(token)
+        if (idToken != null) {
+            val payload: GoogleIdToken.Payload = idToken.payload
+
+            // Print user identifier
+            val userId: String = payload.subject
+            println("User ID: $userId")
+
+            // Get profile information from payload
+            val email: String = payload.email
+            val emailVerified: Boolean = java.lang.Boolean.valueOf(payload.emailVerified)
+            val name = payload["name"] as String
+            val locale = payload["locale"] as String
+            val familyName = payload["family_name"] as String
+            val givenName = payload["given_name"] as String
+
+            println("Name: $name, Family name: $familyName")
+
+            return payload
+        } else {
+            return null;
+        }
+    }
+
+    private fun createAccountFromGooglePayload(payload: GoogleIdToken.Payload, role: Long) {
+        when(role) {
+
+        }
     }
 
     @PostMapping("/change_password")
